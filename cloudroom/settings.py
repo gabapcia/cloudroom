19212f313fd5d353f9736f1a1bf521005e259e77
Gabriel Baptistucci Garcia <gabrielbgarcia0999@gmail.com>
2020-11-01 19:05:47 -0300

    orders app removed

--- cloudroom/settings.py
+++ cloudroom/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'microcontrollers',
-    'orders',
 ]
 
 MIDDLEWARE = [
--- orders/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from orders import serializers, models
-
-
-class CorreiosViewSet(viewsets.ModelViewSet):
-    queryset = models.Correios.objects.order_by('-updated')
-    serializer_class = serializers.CorreiosSerializer
